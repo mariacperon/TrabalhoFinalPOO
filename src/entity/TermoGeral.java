a688419c235bcb08bc78b4fed7886f5bd66f93b6
mariacperon <mariaclaraperon@gmail.com>
2023-11-21 13:04:20 -0300

    maria: finalizando implementação dos métodos de salvar e recuperar dados por arquivo

--- src/entity/TermoGeral.java
+++ src/entity/TermoGeral.java
@@ -4,7 +4,13 @@
  */
 package entity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,4 +24,41 @@ public class TermoGeral extends Termo implements Serializable{
         super(nome, descricao);
     }
     
+    
+    public void cadastrarTermosGerais(List<TermoGeral> termosGerais) {
+        List<TermoGeral> termosGeraisNew = retornaTermosGerais();
+        
+        System.out.println("iniciando salvar TermoGeral");
+        termosGeraisNew.addAll(termosGerais);
+        
+        salvarTermoGeral(termosGeraisNew);
+        
+        System.out.println("finalizando salvar TermoGeral");
+    }
+    
+    private void salvarTermoGeral(List<TermoGeral> termosGerais) {        
+        File arquivo = new File("ect\\Geral.dat");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(arquivo))) {
+            oos.writeObject(termosGerais);
+            oos.flush();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public List<TermoGeral> retornaTermosGerais() {
+        List<TermoGeral> termosGerais = new ArrayList<>();
+        File arquivo = new File("ect\\Geral.dat");
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arquivo))) {
+            List<TermoGeral> termosGeraisRetorno = (List<TermoGeral>) ois.readObject();
+            
+            for (TermoGeral termoGeral : termosGeraisRetorno) {
+                termosGerais.add(termoGeral);
+            }
+        } catch (Exception ex) {
+            return termosGerais;
+        }
+        
+        return termosGerais;
+    }
 }
